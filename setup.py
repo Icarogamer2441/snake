c85704b4fb3b6aa88f94d94709affe716c6e04d2
Icarogamer2441 <deiamota050@gmail.com>
2025-04-01 20:40:36 -0300

    updating __version__ to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="snake-lang",
-    version="0.1.0",
+    version="0.2.0",
     description="A statically typed superset of Python",
     author="Snake Team",
     packages=find_packages(),
